88671e4db5e5dee4c250bf09ee72c3ee7c358359
Dallin Brimhall <dallinbrimhall@gmail.com>
2017-08-31 11:16:25 -0600

    Starting Screen
    Game start up lines

---
+++ GuessingGame/Assets/Guessing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guessing : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		print ("Welcome to Nuber Guesser!");
+		print ("Pick a number in your head. Don't tell me what it is.");
+
+		print ("The highest number you can pick is 500.");
+		print ("The lowest number you can pick is 1.");
+
+		print ("Is the number higher or lower than 50?");
+		print ("Up arrow for higher. Down arrow for Lower. = for I got it right.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
